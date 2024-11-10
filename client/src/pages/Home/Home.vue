16cc2031394c9a52d47bcd9ad47d47f465494e85
darienmiller88 <darienmiller88@yahoo.com>
2024-11-10 03:46:58 -0500

    added mission Statment container

--- client/src/pages/Home/Home.vue
+++ client/src/pages/Home/Home.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
     import Hero from '../../containers/Hero/Hero.vue';
     import Description from '../../containers/Description/Description.vue';
+    import MissionStatement from '../../containers/MissionStatement/MissionStatement.vue';
 </script>
 
 <template>
     <Hero />
     <Description />
+    <MissionStatement />
 </template>
 
 <style scoped>
