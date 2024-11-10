b9767c3c02fd13a263f632fee7384f5944e49b7e
Fathia05 <93850624+Fathia05@users.noreply.github.com>
2024-11-09 23:13:45 -0600

    Added Footer

--- client/src/pages/AboutUs/AboutUs.vue
+++ client/src/pages/AboutUs/AboutUs.vue
@@ -3,9 +3,54 @@
 </script>
 
 <template>
-    <h1>
-        About us
-    </h1>
+    <div class="about-section">
+        <h1>About us</h1>
+        <p>A little intro to the team.</p>
+    </div>
+
+    <h2>Our Team</h2>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="" alt="image " style="width:100%">
+                <div class="container">
+                    <h2>Jane Doe</h2>
+                    <p class="title">CEO & Founder</p>
+                    <p>School attended</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="" alt="" style="width:100%">
+                <div class="container">
+                    <h2>Jane Doe</h2>
+                    <p class="title">CEO & Founder</p>
+                    <p>School attended</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="" alt="" style="width:100%">
+                <div class="container">
+                    <h2>Jane Doe</h2>
+                    <p class="title">CEO & Founder</p>
+                    <p>School attended</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="" alt="" style="width:100%">
+                <div class="container">
+                    <h2>Jane Doe</h2>
+                    <p class="title">CEO & Founder</p>
+                    <p>School attended</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
