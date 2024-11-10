a8a80cbe2e8ae66f23784fa270cab766fdad9fe2
darienmiller88 <darienmiller88@yahoo.com>
2024-11-10 04:09:19 -0500

    WorkflowContainer done

---
+++ client/src/containers/WorkflowContainer/WorkflowContainer.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+    import image4 from "../../../public/home_pictures/image4-removebg-hq.png"
+</script>
+
+<template>
+    <div class="workflow-Container">
+        <div class="title">
+            Get Ready to Maximize Your Productivity With Our Workflow Solutions
+        </div>
+        <div class="button-wrapper">
+            <button>Get Started</button>
+        </div>
+        <img :src="image4" alt="image">
+    </div>
+</template>
+
+<style scoped lang="scss"> 
+    .workflow-Container{
+        // border: 3px dotted red;
+
+        .title{
+            text-align: center;
+            margin-top: 100px;
+            padding: 0px 250px;
+            font-size: 40px;
+        }
+
+        .button-wrapper{
+            margin: 30px;
+            text-align: center;
+
+            button{
+                color: white;
+                font-weight: bold;
+                transition: 0.3s;
+
+                &:hover{
+                    background-color: var(--goldenrod);
+                    color: black;
+                }
+            }
+        }
+
+        img{
+            width: 100%;
+            height: auto;
+        }
+    }
+</style>
--- client/src/pages/Home/Home.vue
+++ client/src/pages/Home/Home.vue
@@ -2,12 +2,14 @@
     import Hero from '../../containers/Hero/Hero.vue';
     import Description from '../../containers/Description/Description.vue';
     import MissionStatement from '../../containers/MissionStatement/MissionStatement.vue';
+    import WorkflowContainer from '../../containers/WorkflowContainer/WorkflowContainer.vue';
 </script>
 
 <template>
     <Hero />
     <Description />
     <MissionStatement />
+    <WorkflowContainer />
 </template>
 
 <style scoped>
