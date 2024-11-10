65501b67bed713eb0e0eefee4bd449e2223b2ae0
Christopher Yuen <chriskaijoyu@gmail.com>
2024-11-09 22:50:59 -0500

    some edits

--- client/src/pages/AboutUs/AboutUs.vue
+++ client/src/pages/AboutUs/AboutUs.vue
@@ -6,10 +6,18 @@
     <h1>
         About us
     </h1>
+    <div class="content-box">
+        <p>
+            Welcome to our application! We are dedicated to providing the best service possible.
+        </p>
+    </div>
 </template>
 
 <style scoped>
     h1{
         color: antiquewhite;
+    }
+    p{
+        text-align: center;
     }
 </style>
