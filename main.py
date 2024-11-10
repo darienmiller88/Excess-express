e47c03518e34a1a97f5213cd1b8cff92d3d851be
darienmiller88 <darienmiller88@yahoo.com>
2024-11-09 21:59:25 -0500

    change lol

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+# Enable CORS for specific domains, methods, and headers
+CORS(app, resources={
+    r"/*": {
+        "origins": "http://localhost:5000",  # Allow only requests from example.com
+        "methods": ["GET", "POST"],       # Allow only GET and POST methods
+        "allow_headers": ["Authorization", "Content-Type"]  # Allow specific headers
+    }
+})
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello world"
 
 if __name__ == "__main__":
     app.run(debug=True)
